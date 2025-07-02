ff2a40df1db69e2d0f7148bdaf18c267f194a1f2
DimphoSetsile <132808119+DimphoSetsile@users.noreply.github.com>
2025-07-02 07:13:48 +0200

    Added some comments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React,{useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import './Navbar.css';
-import {Button} from "./Button.js";
+import {Button} from "./Button";
 
 
 
@@ -22,6 +22,7 @@ function Navbar(){
         }
         
     };
+    
     window.addEventListener('resize',showButton);
 
     useEffect(()=>{
@@ -33,25 +34,30 @@ function Navbar(){
         <nav className="navbar">
         <div className="navbar-container">
             <Link to="/" className="navbar-logo">
-            Travel <i classname='fab fa-typo3' />
+            
+            Psychological thriller hub  <i classname='fab fa-book' />
             </Link>
+            {/* when you click the menu icon, it shows the menu bars content*/}
             <div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fas fa-times': 'fas fa-bars'} > </i>
                 
             </div>
             <ul className={click ? 'nav-menu active': 'nav-menu'}>
+                {/*on click menu closes and redirects to link */}
                 <li className='nav-item'>
                     <Link to='/home' className='nav-links' onClick={closeMobileMenu}>
                      Home
                     </Link>
 
                 </li>
+                {/*on click menu closes and redirects to link*/}
                 <li className='nav-item'>
                     <Link to='/sevices' className='nav-links' onClick={closeMobileMenu}>
                      services
                     </Link>
 
                 </li>
+                {/*on click menu closes and redirects */}
                 <li className='nav-item'>
                     <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
                      Products
