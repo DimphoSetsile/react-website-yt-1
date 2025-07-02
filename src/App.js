fe4e18c1acca1221ad54c12266c8c0392e87b1da
DimphoSetsile <132808119+DimphoSetsile@users.noreply.github.com>
2025-07-02 06:22:12 +0200

    imported font awesome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import Home from './components/Pages/Home';
 import Services from './components/Pages/Services';
 import Products from './components/Pages/Products';
 import SignUp from './components/Pages/SignUp';
+
 
 function App() {
   return (
@@ -13,7 +16,9 @@ function App() {
     <Router>
       <Navbar />
       
+      
       <Routes>
+        {/*links on the navbar*/}
         <Route path='/Home' element={<Home/>} />
         <Route path='/Services' element={<Services/>} />
          <Route path='/Products' element={<Products/>} />
@@ -23,6 +28,8 @@ function App() {
         
       
       </Routes>
+      
+      <Footer />
      </Router>
     </>
   );
